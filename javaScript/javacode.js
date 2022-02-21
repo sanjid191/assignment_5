25c69dce25a9a6dfb9dc67ff82ef74243489b78d
sanjid191 <sanjid.sa191@gmail.com>
2022-02-21 22:54:01 +0600

    code commited

--- javaScript/javacode.js
+++ javaScript/javacode.js
@@ -1,21 +1,19 @@
 
 function getInput(id) {
   const inputField = document.getElementById(id);
-  const inputFieldText = inputField.value;
-  const inputAmonut = parseFloat(inputFieldText);
+  const textInputField = inputField.value;
+  const inputAmonut = parseFloat(textInputField);
 
   if (isNaN(inputAmonut) || inputAmonut < 0) {
-    return 'Input a valid Number'
+    return 'Input valid Numbers'
   } else {
     return inputAmonut;
   }
-
 }
 
-function calculateCost(number1, number2, number3) {
+function costCulculation(number1, number2, number3) {
   return number1 + number2 + number3;
 }
-
 
 function finalBalance(income, cost) {
   return income - cost;
@@ -24,17 +22,16 @@ function finalBalance(income, cost) {
 document.getElementById('calculate-Button').addEventListener('click', function () {
 
   const incomeValue = getInput('input-income');
-  const foodValue = getInput('cost-food');
-  const rentValue = getInput('cost-rent');
-  const clotheValue = getInput('cost-clothe');
+  const totalValueOfFood = getInput('cost-food');
+  const totalValueOfrent = getInput('cost-rent');
+  const totalValueOfcloth = getInput('cost-clothe');
 
-
-  if (typeof (foodValue) == 'string' || typeof (rentValue) == 'string' || typeof (clotheValue) == 'string') {
+  if (typeof (totalValueOfFood) == 'string' || typeof (totalValueOfrent) == 'string' || typeof (totalValueOfcloth) == 'string') {
     document.getElementById('expenses-text').style.display = 'none';
     document.getElementById('failed-balance').style.display = 'none';
-    document.getElementById('total-expenses').innerText = `❌ Please input valid number`;
+    document.getElementById('total-expenses').innerText = ` Please input valid number`;
   } else {
-    const totalCost = calculateCost(foodValue, rentValue, clotheValue)
+    const totalCost = costCulculation(totalValueOfFood, totalValueOfrent, totalValueOfcloth)
     document.getElementById('total-expenses').innerText = totalCost;
     document.getElementById('expenses-text').style.display = 'block';
     document.getElementById('failed-balance').style.display = 'block';
@@ -43,16 +40,15 @@ document.getElementById('calculate-Button').addEventListener('click', function (
   if (typeof (incomeValue) == 'string') {
     document.getElementById('expenses-text').style.display = 'none';
     document.getElementById('failed-balance').style.display = 'none';
-    document.getElementById('total-expenses').innerText = `❌ Please input valid number`;
-  } else if (incomeValue < calculateCost(foodValue, rentValue, clotheValue)) {
+    document.getElementById('total-expenses').innerText = ` Please input valid number`;
+  } else if (incomeValue < costCulculation(totalValueOfFood, totalValueOfrent, totalValueOfcloth)) {
     document.getElementById('expenses-text').style.display = 'none';
     document.getElementById('failed-balance').style.display = 'none';
-    document.getElementById('total-expenses').innerText = `❌ cost cannot be more than income`;
+    document.getElementById('total-expenses').innerText = ` cost cannot be more than income`;
   } else {
-    const totalBalance = finalBalance(incomeValue, calculateCost(foodValue, rentValue, clotheValue))
+    const totalBalance = finalBalance(incomeValue, costCulculation(totalValueOfFood, totalValueOfrent, totalValueOfcloth))
     document.getElementById('total-balance').innerText = totalBalance;
   }
-
 
 });
 
@@ -62,19 +58,18 @@ function savingAmount(income, saving) {
 
 function savingCalculation() {
   const incomeValue = getInput('input-income');
-  const savingValue = getInput('saving-input');
+  const savingValue = getInput('input-saving');
   const totalBalanceText = document.getElementById('total-balance').innerText;
   const totalBalance = parseFloat(totalBalanceText);
-
 
   if (typeof (savingValue) == 'string') {
     document.getElementById('saving-text').style.display = 'none';
     document.getElementById('remain-balance').style.display = 'none';
-    document.getElementById('saving-total').innerText = `❌ Please input valid number`;
+    document.getElementById('saving-total').innerText = ` Please input valid number`;
   } else if (totalBalance < savingAmount(incomeValue, savingValue)) {
     document.getElementById('saving-text').style.display = 'none';
     document.getElementById('remain-balance').style.display = 'none';
-    document.getElementById('saving-total').innerText = `❌ Impossible to save money more than Balance`;
+    document.getElementById('saving-total').innerText = ` Impossible to save money more than Balance`;
   } else {
     document.getElementById('saving-text').style.display = 'block';
     document.getElementById('remain-balance').style.display = 'block';
